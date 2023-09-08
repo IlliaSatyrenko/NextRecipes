35a787a562f2a0218b7b34eeb22f8588394192d5
IlliaSatyrenko <denzp05@gmail.com>
2023-09-08 19:10:46 +0300

    add files

---
+++ src/app/item/[id]/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import styles from "./ItemPage.module.css";
+
+// import useFetch from "../../hooks/Fetch";
+
+import Header from "@/app/Components/Header/Header";
+import Footer from "@/app/Components/Footer/Footer";
+import Video from "../../Components/Video";
+
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { Container, Link } from "@mui/material";
+
+function ItemPage({ params: { id } }: { params: { id: string } }) {
+    // const navigate = useNavigate();
+
+    const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id;
+    const currentData = useRef<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const measures: string[] = [];
+    const ingredients: string[] = [];
+    let videoId: string = "";
+
+    useEffect(() => {
+        console.log(url);
+        axios
+            .get(url)
+            .then((response) => {
+                if (response !== undefined) {
+                    currentData.current = response.data.meals[0];
+                } else {
+                    console.log("NETWORK ERROR");
+                }
+            })
+            .then(() => {
+                console.log(currentData);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                throw new Error("Error" + error);
+            });
+    }, [url]);
+
+    // useFetch(url).then((response) => {
+    //     if (response !== undefined) {
+    //         currentData.current = response;
+    //         if (currentData.current) {
+    //             currentData.current = currentData.current[0];
+    //         }
+    //     } else {
+    //         console.log("NETWORK ERROR");
+    //     }
+    // });
+    // console.log(currentData.current);
+
+    if (currentData.current !== undefined) {
+        const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        const match = currentData.current.strYoutube?.match(regExp);
+        if (match && match[7].length === 11) {
+            videoId = match[7];
+        }
+
+        for (let i = 1; i < 21; i++) {
+            let str = "strIngredient" + i;
+            if (!currentData.current[str] || currentData.current[str] == " ") break;
+            const fChar = currentData.current[str].charAt(0).toUpperCase();
+            const rChars = currentData.current[str].slice(1);
+            ingredients.push(fChar + rChars);
+        }
+
+        for (let i = 1; i < 21; i++) {
+            let str = "strMeasure" + i;
+            if (!currentData.current[str] || currentData.current[str] == " ") break;
+            const fChar = currentData.current[str].charAt(0).toUpperCase();
+            const rChars = currentData.current[str].slice(1);
+            measures.push(fChar + rChars);
+        }
+    }
+
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                {isLoading ? (
+                    <></>
+                ) : (
+                    <Link href="/all-items" className={styles.itemButton} color="#ffa135">
+                        All items
+                    </Link>
+                )}
+                <Container className={styles.wrapper} maxWidth="lg">
+                    {isLoading ? (
+                        <h1>LOADING...</h1>
+                    ) : (
+                        <>
+                            <div className={styles.itemHeader}>
+                                <span className={styles.itemCategory}>{currentData.current.strCategory}</span>
+                                <h1 className={styles.itemName}>{currentData.current.strMeal}</h1>
+                            </div>
+                            <div className={styles.itemVideo}>
+                                <Video videoId={videoId} />
+                            </div>
+                            <div className={styles.itemInfo}>
+                                {currentData.current.strTags && (
+                                    <div className={styles.itemTags}>
+                                        <h3 className={styles.tagsHeader}>Tags</h3>
+                                        <span className={styles.tagsText}>{currentData.current.strTags.replace(/,/g, ", ")}</span>
+                                    </div>
+                                )}
+                                {ingredients[0] && (
+                                    <div className={styles.itemIngredients}>
+                                        <h3 className={styles.ingredientsHeader}>Ingredients</h3>
+                                        {(window.innerWidth > 575 && (
+                                            <>
+                                                <div className={styles.ingredientsWrapper}>
+                                                    {ingredients.map((ingred) => {
+                                                        return <span className={styles.itemIngred}>{ingred}:</span>;
+                                                    })}
+                                                </div>
+                                                <div className={styles.measuresWrapper}>
+                                                    {measures.map((meas) => {
+                                                        return <span className={styles.itemMeasure}>{meas},</span>;
+                                                    })}
+                                                </div>
+                                            </>
+                                        )) || (
+                                            <div className={styles.ingredientsWrapper}>
+                                                {ingredients.map((ingred, index) => {
+                                                    return (
+                                                        <span key={index} className={styles.itemIngred}>
+                                                            {ingred}: {measures[index]}
+                                                        </span>
+                                                    );
+                                                })}
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
+                            <div className={styles.itemInstruction}>
+                                <h3 className={styles.instructionHeader}>Instruction</h3>
+                                <p className={styles.instructionText}>{currentData.current.strInstructions}</p>
+                            </div>
+                        </>
+                    )}
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default ItemPage;
